32f273d351f33d1ce8b409e6ab418d38c0e26c4a
Rufino ✝️ <rufinofelipe495@gmail.com>
2026-02-15 22:58:59 +0100

    Actualizar config.py

--- bot/config.py
+++ bot/config.py
@@ -1,8 +1,8 @@
 import os
 import yaml
 
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "")
-OPENAI_API_KEY     = os.getenv("OPENAI_API_KEY", "")
+TELEGRAM_BOT_TOKEN = os.getenv("8355097375:AAGS1RmvuDeuyIdAK9RLH8n2fGaCPmNJzD8", "")
+OPENAI_API_KEY     = os.getenv("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", "")
 
 OPENAI_MODEL = os.getenv("OPENAI_MODEL", "gpt-4o")
 MAX_TOKENS   = int(os.getenv("MAX_TOKENS", "2000"))
